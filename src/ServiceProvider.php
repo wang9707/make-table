17b05e6ee75f31fbd8ab81d0b20a84ff6c91109f
wang <1005945366@qq.com>
2022-02-18 17:26:18 +0800

    v1

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,15 +2,24 @@
 
 namespace Wang9707\MakeTable;
 
-use Wang9707\MakeTable\Console\CreateModel;
+use Wang9707\MakeTable\Console\MakeModel;
+use Wang9707\MakeTable\Console\MakeService;
+use Wang9707\MakeTable\Console\MakeController;
+use Wang9707\MakeTable\Console\MakeFilter;
+use Wang9707\MakeTable\Console\MakeAll;
 
-class ServiceProvider
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function boot()
     {
         if ($this->app->runningInConsole()) {
             $this->commands([
-                CreateModel::class
+                MakeModel::class,
+                MakeService::class,
+                MakeController::class,
+                MakeFilter::class,
+                MakeAll::class,
             ]);
         }
     }
